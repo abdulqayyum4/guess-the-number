a8df9ed8d852fd916773e629ec4a2dac2e54c15f
Abdul Qayyum <160266329+abdulqayyum4@users.noreply.github.com>
2025-07-11 07:18:21 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+# Guess The Number Game   - User Guesses -3
+
+import random
+
+#Introduction
+number = random.randint(1, 100)
+guesses = 0
+
+while True:
+    # User se input lena
+    user_guess = input("Enter your guess")
+
+    # Input ko integer main convert karna
+    try: 
+        user_guess = int(user_guess)
+        guesses += 1
+        
+        # Guess ko check karna
+        if user_guess < number:
+            print("Too low! Try again")
+        elif user_guess > number:
+            print("Too high! Try again")
+        else:
+            print(f"Congratulations! You guessed the number in {guesses} tries.")
+            break
+    except ValueError:
+        print("Please enter a valid number.")
